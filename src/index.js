ba863cbe31921bba7a0f475f6364b8f63285e14c
Eugene Volkov <eugenewolf507@gmail.com>
2020-09-29 10:28:41 +0300

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        const sortComparison = (a, b) => a - b;
+        const sortedArray = array.sort(sortComparison);
+        return sortedArray[0];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        const sortComparison = (a, b) => b - a;
+        const sortedArray = array.sort(sortComparison);
+        return sortedArray[0];
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        const summ = array.reduce((acc, value) => acc + value, 0);
+        return summ / array.length;
+    }
+};
